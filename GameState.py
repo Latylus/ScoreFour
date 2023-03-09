a5c3c1fa368ceb4d3a530938ad5f99d99efaa5d3
Pierre Lataillade <pierre.lataillade@gmail.com>
2023-03-09 10:57:37 +0100

    Made grid size and win condition parametrizable

--- GameState.py
+++ GameState.py
@@ -1,5 +1,8 @@
+SIZE = 5
+WIN_SIZE = 5
 
-pointGenerators = [(x,y,z) for x in range(4) for y in range(4) for z in range(4) if x == 0 or y == 0 or z == 0]  
+#used for no prior information victory conditions
+pointGenerators = [(x,y,z) for x in range(SIZE) for y in range(SIZE) for z in range(SIZE) if x == 0 or y == 0 or z == 0]  
 directions = [1,0,-1]
 vectors = [(i,j,k) for i in directions for j in directions for k in directions if i!=0 or j!=0 or k!=0 ]
 half_vectors = vectors[0:len(vectors)//2]
@@ -7,40 +10,41 @@ mirror_vectors = list(reversed(vectors[len(vectors)//2 :]))
 
 #This dict allows us to know which points to check for a win condition, whenever the last move is on a given point
 pointsToCheckDic = {}
-print("precomputing")
-for x,y,z in [(x,y,z) for x in range(4) for y in range(4) for z in range(4)]:
+print("precomputing victory checking")
+for x,y,z in [(x,y,z) for x in range(SIZE) for y in range(SIZE) for z in range(SIZE)]:
     p0 = (x,y,z)
     pointsToCheckDic[p0] = []
     for vf, vb in zip(half_vectors, mirror_vectors):
-        p1 = tuple(map(sum, zip(p0, vf)))
-        p2 = tuple(map(sum, zip(p1, vf)))
-        p3 = tuple(map(sum, zip(p2, vf))) 
-        pm1 = tuple(map(sum, zip(p0, vb)))
-        pm2 = tuple(map(sum, zip(pm1, vb)))
-        pm3 = tuple(map(sum, zip(pm2, vb))) 
-        inboundPoints = [p for p in [p0,p1,p2,p3,pm1,pm2,pm3] if all(c>=0 and c<4 for c in p)]
-        if len(inboundPoints) >= 4 :
+        pointList = [p0]
+        lastF = p0
+        lastB= p0
+        for _ in range(WIN_SIZE-1):
+            lastF = tuple(map(sum, zip(lastF, vf)))
+            lastB = tuple(map(sum, zip(lastB, vb)))
+            pointList += [lastF, lastB]
+        inboundPoints = [p for p in pointList if all(c>=0 and c<SIZE for c in p)]
+        if len(inboundPoints) >= WIN_SIZE :
             pointsToCheckDic[p0] = pointsToCheckDic[p0] + [inboundPoints]
-
-print("done precomputing")
+print("done precomputing victory checking")
 
 class GameState:
-    Grid = [[[None for k in range(4)] for j in range(4)] for i in range(4)]    
+    Grid = [] 
     IsPlayerZeroTurn = True
     LastMove = None
     MoveCount = 0
 
     def __init__(self) -> None:
-        self.Grid = [[[None for k in range(4)] for j in range(4)] for i in range(4)]    
+        self.Grid = [[[None for k in range(SIZE)] for j in range(SIZE)] for i in range(SIZE)]    
         self.IsPlayerZeroTurn = True
+        self.LastMove = None
         self.MoveCount = 0
 
     def getPossibleMoves(self) -> list:
-        return [(x,y) for x in range(4) for y in range(4) if None in self.Grid[x][y]] #None means a peg spot is empty
+        return [(x,y) for x in range(SIZE) for y in range(SIZE) if None in self.Grid[x][y]] #None means a peg spot is empty
 
 
     def checkEnd(self) -> bool : 
-        return self.getWinner() is not None or self.MoveCount == 64
+        return self.getWinner() is not None or self.MoveCount == SIZE**3
     
 
     def getWinner(self) -> int:
@@ -56,27 +60,29 @@ class GameState:
             return None
         for vf, vb in zip(half_vectors, mirror_vectors):
             p0 = self.LastMove
-            p1 = tuple(map(sum, zip(p0, vf)))
-            p2 = tuple(map(sum, zip(p1, vf)))
-            p3 = tuple(map(sum, zip(p2, vf))) 
-            pm1 = tuple(map(sum, zip(p0, vb)))
-            pm2 = tuple(map(sum, zip(pm1, vb)))
-            pm3 = tuple(map(sum, zip(pm2, vb))) 
-            inboundPoints = [p for p in [p0,p1,p2,p3,pm1,pm2,pm3] if all(c>=0 and c<4 for c in p)]
+            pointList = [p0]
+            lastF = p0
+            lastB = p0
+            for _ in range(WIN_SIZE-1):
+                lastF = tuple(map(sum, zip(lastF, vf)))
+                lastB = tuple(map(sum, zip(lastB, vb)))
+                pointList += [lastF, lastB]
+            inboundPoints = [p for p in pointList if all(c>=0 and c<SIZE for c in p)]
             lastMoveValue = self.Grid[p0[0]][p0[1]][p0[2]]
-            if len(inboundPoints) >= 4 and all(V == lastMoveValue for V in [self.Grid[x][y][z] for (x,y,z) in inboundPoints]) :
+            if len(inboundPoints) >= WIN_SIZE and all(V == lastMoveValue for V in [self.Grid[x][y][z] for (x,y,z) in inboundPoints]) :
                 return lastMoveValue 
         return None
 
     def getWinnerWithNoPriorInfo(self) -> int:
         for (p,v) in [(point,vector) for point in pointGenerators for vector in vectors] :
-            p0 = p
-            p1 = tuple(map(sum, zip(p0, v)))
-            p2 = tuple(map(sum, zip(p1, v)))
-            p3 = tuple(map(sum, zip(p2, v)))
-            points = [p0,p1,p2,p3]
-            edgeValue = self.Grid[p0[0]][p0[1]][p0[2]]
-            if edgeValue is not None and all(all(c>=0 and c<4 for c in p) for p in points) and all(edgeValue == V for V in [self.Grid[x][y][z] for (x,y,z) in points]) :
+            lastP = p
+            pointList = [p]
+            for _ in range(WIN_SIZE-1):
+                lastP = tuple(map(sum, zip(lastP, v)))
+                pointList += [lastP]
+            
+            edgeValue = self.Grid[p[0]][p[1]][p[2]]
+            if edgeValue is not None and all(all(c>=0 and c<SIZE for c in p) for p in pointList) and all(edgeValue == V for V in [self.Grid[x][y][z] for (x,y,z) in pointList]) :
                 return edgeValue 
         return None
     
